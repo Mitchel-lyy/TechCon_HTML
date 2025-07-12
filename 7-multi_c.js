546ea8678dddd0206d27ff3795b9c33ac31a095f
Mitchell <mitchelleleojomicheal@gmail.com>
2025-07-12 21:25:44 +0100

    new file added

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,6 +1,6 @@
 const countArg = process.argv[2];
 const numOccurrences = parseInt(countArg);
-if (isNaN(numOccurrences)|| numOccurrences <= 0) {
+if (isNaN(numOccurrences) && numOccurrences > 0) {
     console.log("Missing number of occurrences");
 } else {
     for (let i = 0; i < numOccurrences; i++) {
