6882130aa09d1bc6870384fec39ce60cb16b19cb
Mitchell <mitchelleleojomicheal@gmail.com>
2025-07-12 18:57:31 +0100

    Add 6-multi_languages_loop.js using array and loop

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,4 +1,4 @@
-const languages = ["C is fun", "python is cool", "Javascript is amazing"];
+const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
 for (let i = 0; i < languages.length; i++) {
     console.log(languages[i]);
 }
